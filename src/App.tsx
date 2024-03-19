eebf2a265ad0ac256b0c67ea846b0f79194f5e50
PRMotorWorks <lucathomasfaur@gmail.com>
2024-03-19 10:10:20 -0400

    Update v3

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 import BMWM4 from "./assets/BMWM4.jpg"
 function App() {
 return <main>
-  <h1>drmrincredibleamazing</h1><img
+  <h1>The most powerful BMW M model is the 2024 BMW XM, which has a 4.4-liter V-8 engine that produces 644 horsepower and can accelerate from 0 to 60 mph in 3.3 seconds. This performance makes you wanna</h1><img
 src={BMWM4}
 />
 </main>
